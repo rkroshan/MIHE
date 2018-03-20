9d8bbb745891eba822a958b9bda9cde9b0b60085
rkroshan <rkroshan.1999@gmail.com>
2018-03-21 03:27:10 +0530

    Explorer Added

---
+++ app/src/main/java/cubes/logic/mihe/Feed/Explore.java
@@ -0,0 +1,60 @@
+package cubes.logic.mihe.Feed;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import cubes.logic.mihe.R;
+
+/**
+ * Created by CREATOR on 3/21/2018.
+ */
+
+public class Explore extends Fragment {
+
+
+    private RecyclerView explore_recyclerview;
+    private DisplayMetrics displayMetrics;
+    private int height;
+    private static ArrayList<String> mlist = new ArrayList<>();
+    static {
+        mlist.add("IDEAS");
+        mlist.add("PRODUCTS");
+        mlist.add("MOTIVATIONS");
+        mlist.add("RESOURCES");
+        mlist.add("EVENTS");
+        mlist.add("SCHEMES");
+    }
+    GridLayoutManager gridLayoutManager;
+    private int width=0;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.explore_frag,container,false);
+        getScreenSize();
+        explore_recyclerview = view.findViewById(R.id.explore_recyclerview);
+        gridLayoutManager = new GridLayoutManager(getContext(),2);
+        explore_recyclerview.setLayoutManager(gridLayoutManager);
+        Log.e("mlist----",mlist.get(0));
+        explore_recyclerview.setAdapter(new ExploreAdapter(mlist,getContext()));
+        return view;
+    }
+
+    private void getScreenSize() {
+        displayMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        height = displayMetrics.heightPixels;
+        width = displayMetrics.widthPixels;
+        Log.e("mlist----",height+"");
+    }
+}
--- app/src/main/java/cubes/logic/mihe/FeedFragment.java
+++ app/src/main/java/cubes/logic/mihe/FeedFragment.java
@@ -2,10 +2,20 @@ package cubes.logic.mihe;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cubes.logic.mihe.Feed.Explore;
+import cubes.logic.mihe.Feed.Feed;
 
 
 /**
@@ -13,17 +23,67 @@ import android.view.ViewGroup;
  */
 public class FeedFragment extends Fragment {
 
-
     public FeedFragment() {
         // Required empty public constructor
     }
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+        ViewPager feed_viewPager = view.findViewById(R.id.feed_viewPager);
+        setupViewPager(feed_viewPager);
+        TabLayout feed_tabs = view.findViewById(R.id.feed_tabs);
+        feed_tabs.setTabGravity(TabLayout.GRAVITY_FILL);
+        feed_tabs.setupWithViewPager(feed_viewPager);
+        return view;
     }
+
+
+    private void setupViewPager(ViewPager viewPager) {
+
+
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new Feed(), "Feed");
+        adapter.addFragment(new Explore(), "Explore");
+        viewPager.setAdapter(adapter);
+
+
+
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
 
 }
