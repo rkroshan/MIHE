41ec601565cd6a395f9c7c1c830be2b627e96e67
Ritu Raj <rituraj22in@gmail.com>
2018-03-20 23:44:28 +0530

    Added all fragments

---
+++ app/src/main/java/cubes/logic/mihe/MainViewPagerAdapter.java
@@ -0,0 +1,10 @@
+package cubes.logic.mihe;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by cogito on 3/20/18.
+ */
+
+public class MainViewPagerAdapter extends FragmentPagerAdapter {
+}
