37c143fdaefeb5b7124c4f9c11e25997c42fd2cc
rkroshan <rkroshan.1999@gmail.com>
2018-03-28 14:33:42 +0530

    chat_initial_phase

--- app/src/main/java/cubes/logic/mihe/Assistant/chat_model.java
+++ app/src/main/java/cubes/logic/mihe/Assistant/chat_model.java
@@ -1,18 +1,47 @@
 package cubes.logic.mihe.Assistant;
 
+import java.util.ArrayList;
+
 /**
  * Created by CREATOR on 3/28/2018.
  */
 
 public class chat_model {
-    private String IMAGEDATA,QUESTION,OPTION1,OPTION2,OPTION3,OPTION4,OPTION5;
+    private String IMAGEDATA, QUESTION, OPTION_YES, OPTION_NO, OPTION1, OPTION2, OPTION3, OPTION4;
+
+    public chat_model(String IMAGEDATA, String QUESTION, String OPTION_YES, String OPTION_NO, String OPTION1, String OPTION2, String OPTION3, String OPTION4) {
+        this.IMAGEDATA = IMAGEDATA;
+        this.QUESTION = QUESTION;
+        this.OPTION_YES = OPTION_YES;
+        this.OPTION_NO = OPTION_NO;
+        this.OPTION1 = OPTION1;
+        this.OPTION2 = OPTION2;
+        this.OPTION3 = OPTION3;
+        this.OPTION4 = OPTION4;
+    }
+
+    public String getOPTION_YES() {
+        return OPTION_YES;
+    }
+
+    public void setOPTION_YES(String OPTION_YES) {
+        this.OPTION_YES = OPTION_YES;
+    }
+
+    public String getOPTION_NO() {
+        return OPTION_NO;
+    }
+
+    public void setOPTION_NO(String OPTION_NO) {
+        this.OPTION_NO = OPTION_NO;
+    }
 
     public String getIMAGEDATA() {
         return IMAGEDATA;
     }
 
-    public void setIMAGEDATA(String IMAGE) {
-        this.IMAGEDATA = IMAGE;
+    public void setIMAGEDATA(String IMAGEDATA) {
+        this.IMAGEDATA = IMAGEDATA;
     }
 
     public String getQUESTION() {
@@ -55,11 +84,26 @@ public class chat_model {
         this.OPTION4 = OPTION4;
     }
 
-    public String getOPTION5() {
-        return OPTION5;
-    }
+    public static final class CreateData {
 
-    public void setOPTION5(String OPTION5) {
-        this.OPTION5 = OPTION5;
+        public static ArrayList<chat_model> mlist = new ArrayList<>();
+
+        static {
+            mlist.add(new chat_model(null, "Do you have an idea ? ", "YES", "NO", null, null, null, null));
+            mlist.add(new chat_model(null, "Do you know how to implement that idea ? ", "YES", "NO", null, null, null, null));
+            mlist.add(new chat_model(null, "Do you have ambition to become a leader", "YES, Absolutely", "No, I want somthing else", null, null, null, null));
+            mlist.add(new chat_model(null, "Do you have a team ?", "YES", "NO, but i want it", null, null, null, null));
+            mlist.add(new chat_model(null, "We provide resources required to accomplish your idea", null, null, null, null, null, null));
+            mlist.add(new chat_model(null, "We provide a variety of resources", null, null, null, null, null, null));
+            mlist.add(new chat_model(null, "Well you can get hire, create your team or invest in a product through our app", null, null, null, null, null, null));
+            mlist.add(new chat_model(null, "our app gives you a opportunity to get your idea to incubation center, Do you want to enter in it?", "Yes, Of Course", "No", null, null, null, null));
+            mlist.add(new chat_model(null, "You can find people of your interest in our E-NETWORK", null, null, null, null, null, null));
+            mlist.add(new chat_model(null, null, null, null, null, null, null, null));
+            mlist.add(new chat_model(null, "Well we can help you in these ways", null, null, null, null, null, null));
+        }
+
+        public static ArrayList<chat_model> getlist(){
+            return mlist;
+        }
     }
 }
