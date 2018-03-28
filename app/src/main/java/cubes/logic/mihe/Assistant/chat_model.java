8dcdbf89fdff9aff23f0eca3b4cee7eba12f5416
rkroshan <rkroshan.1999@gmail.com>
2018-03-28 11:54:14 +0530

    improved Explore fragment

---
+++ app/src/main/java/cubes/logic/mihe/Assistant/chat_model.java
@@ -0,0 +1,65 @@
+package cubes.logic.mihe.Assistant;
+
+/**
+ * Created by CREATOR on 3/28/2018.
+ */
+
+public class chat_model {
+    private String IMAGEDATA,QUESTION,OPTION1,OPTION2,OPTION3,OPTION4,OPTION5;
+
+    public String getIMAGEDATA() {
+        return IMAGEDATA;
+    }
+
+    public void setIMAGEDATA(String IMAGE) {
+        this.IMAGEDATA = IMAGE;
+    }
+
+    public String getQUESTION() {
+        return QUESTION;
+    }
+
+    public void setQUESTION(String QUESTION) {
+        this.QUESTION = QUESTION;
+    }
+
+    public String getOPTION1() {
+        return OPTION1;
+    }
+
+    public void setOPTION1(String OPTION1) {
+        this.OPTION1 = OPTION1;
+    }
+
+    public String getOPTION2() {
+        return OPTION2;
+    }
+
+    public void setOPTION2(String OPTION2) {
+        this.OPTION2 = OPTION2;
+    }
+
+    public String getOPTION3() {
+        return OPTION3;
+    }
+
+    public void setOPTION3(String OPTION3) {
+        this.OPTION3 = OPTION3;
+    }
+
+    public String getOPTION4() {
+        return OPTION4;
+    }
+
+    public void setOPTION4(String OPTION4) {
+        this.OPTION4 = OPTION4;
+    }
+
+    public String getOPTION5() {
+        return OPTION5;
+    }
+
+    public void setOPTION5(String OPTION5) {
+        this.OPTION5 = OPTION5;
+    }
+}
