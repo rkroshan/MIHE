20a5582cae400fc4aef5ce06db37a7ccafeb214e
rkroshan <rkroshan.1999@gmail.com>
2018-03-29 23:41:10 +0530

    Assistant Improved

---
+++ app/src/main/java/cubes/logic/mihe/SignIn/SignIn.java
@@ -0,0 +1,20 @@
+package cubes.logic.mihe.SignIn;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import cubes.logic.mihe.R;
+
+/**
+ * Created by CREATOR on 3/29/2018.
+ */
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+}
